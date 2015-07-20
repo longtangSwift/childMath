42e62bb94b5503ffe3b0a565a9d7c20b7bc7516c
CT MacBook Pro <CTMacBookPro@pei-macbook.local>
2015-07-20 11:20:07 -0700

    Initial Commit

---
+++ ChildMathTests/ChildMathTests.swift
@@ -0,0 +1,36 @@
+//
+//  ChildMathTests.swift
+//  ChildMathTests
+//
+//  Created by CT MacBook Pro on 7/20/15.
+//  Copyright © 2015 CT MacBook Pro. All rights reserved.
+//
+
+import XCTest
+@testable import ChildMath
+
+class ChildMathTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
